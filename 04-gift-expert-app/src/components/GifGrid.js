cba2366986f20a2417c3eef96301dc894a22f9b8
Santiago Bermudez <s.bermudez@globant.com>
2020-08-06 08:56:22 -0500

    commit message

--- 04-gift-expert-app/src/components/GifGrid.js
+++ 04-gift-expert-app/src/components/GifGrid.js
@@ -1,26 +1,15 @@
 import React from 'react'
 import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
-
-    const {data, loading} = useFetchGifs();
-
- //que se ejecute esa funcion cuando se carga el cmp la primera vez
-    // const [images, setImages] = useState([]);
-    // useEffect(() => { 
-    //     getGifs(category)
-    //                 .then(imgs => setImages(imgs))
-    // }, [category])
-    
-
+    const {data:images, loading} = useFetchGifs(category);
 
     return (
         <>
         <h3>{category}</h3>
-        {loading ? 'Cargando....': 'Data cargada'}
-        {/* <div className="card-grid">
+        <h3>{loading && <p>loading</p>}</h3>
+        <div className="card-grid">
             {
                 images.map((img) =>(
                     <GifGridItem 
@@ -31,7 +20,7 @@ export const GifGrid = ({category}) => {
             }
             
 
-        </div> */}
+        </div>
         </>
         
     )
--- 04-gift-expert-app/src/hooks/useFetchGifs.js
+++ 04-gift-expert-app/src/hooks/useFetchGifs.js
@@ -1,11 +1,26 @@
-import { useState } from "react"
-
-export const useFetchGifs = () => {
+import { useState, useEffect } from "react"
+import {getGifs} from '../helpers/getGifs'
+export const useFetchGifs = (category) => {
     const [state, setstate] = useState({
         data: [],
         loading: true
     });
-    return state;/*
-   {data: [],
-        loading: true}*/
+    //esto es un custom hook
+    useEffect( () => {
+        getGifs(category)
+            .then(imgs =>{
+
+                setTimeout(() => {
+                    setstate({
+                        data: imgs,
+                        loading: false
+                    })
+                }, 1000);
+                
+            })
+
+
+    },[category])
+    return state;
+    
 }
